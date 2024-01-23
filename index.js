c1dc3e7318492f373e319fc40326d4eec3e194ec
NawfalNl <157287463+NawfalNl@users.noreply.github.com>
2024-01-23 14:19:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const rl = createInterface(process.stdin, process.stdout)
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname)
 
-say('Nour\n MD', { font: 'chrome', align: 'center', gradient: ['red', 'magenta'] })
-say('BY NOUREDDINE OUAFY', { font: 'console', align: 'center', gradient: ['red', 'magenta'] })
+say('Nawfal\n MD', { font: 'chrome', align: 'center', gradient: ['red', 'magenta'] })
+say('BY Nawfal nl', { font: 'console', align: 'center', gradient: ['red', 'magenta'] })
 
 var isRunning = false
 /**
